3120f4e459b4bcd87a45ccf942a7d2d32e8ecd5f
log.Dariel <log.Dariel@gmail.com>
2021-12-16 14:36:42 +0100

    run fail

--- configure.sh
+++ configure.sh
@@ -14,25 +14,7 @@ echo -n "${HTCONDOR_PASSWORD}" | sh -c "condor_store_cred add -c -i -"
 # Now issue myself a token.
 sh -c "umask 0077; condor_token_create -identity condor@${CENTRAL_MANAGER} > /etc/condor/tokens.d/condor@${CENTRAL_MANAGER}"
 
-#start service
-if [ "$(ps --pid 1 -o comm -h)" != 'systemd' ] || ! command_exists systemctl; then
-		echo -e "\n# Start the HTCondor service in the background (without systemd)"
-		(
-			if ! is_dry_run; then
-				set -x
-			fi
-			$sh_c "condor_master"
-		)
-	else
-		echo -e "\n# Start the HTCondor service via systemd"
-		(
-			if ! is_dry_run; then
-				set -x
-			fi
-			$sh_c "systemctl enable condor"
-			$sh_c "systemctl start condor"
-		)
-fi
-
+systemctl enable condor
+systemctl start condor
 
 ./run.sh
